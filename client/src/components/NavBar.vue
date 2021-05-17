a6e9f3f42d98ea48b59b26c2bfe0f9453a47a161
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2021-05-18 11:25:05 +1200

    Remove commented code

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -22,47 +22,11 @@
 
 <script>
 export default {
-  name: "NavBar",
-
-  methods: {
-    // isActiveItem: function (routeName) {
-    //   this.$route
-    // }
-  }
+  name: "NavBar"
 }
 </script>
 
 <style scoped>
-/*.navbar-nav>li>a.router-link-exact-active {*/
-/*  color: #555;*/
-/*  background-color: #e7e7e7;*/
-/*}*/
-/*.nav>a.router-link-exact-active {*/
-/*  color: red;*/
-/*}*/
 
-/*.navbar-nav li {*/
-/*  float: left;*/
-/*}*/
 
-/*.nav li {*/
-/*  position: relative;*/
-/*  display: block;*/
-/*}*/
-
-/*.navbar-default .navbar-nav li > a {*/
-/*  color: #777;*/
-/*}*/
-
-/*.navbar-nav li>a {*/
-/*  padding-top: 10px;*/
-/*  padding-bottom: 10px;*/
-/*  line-height: 20px;*/
-/*}*/
-
-/*.nav li>a {*/
-/*  position: relative;*/
-/*  display: block;*/
-/*  padding: 15px 15px;*/
-/*}*/
 </style>
