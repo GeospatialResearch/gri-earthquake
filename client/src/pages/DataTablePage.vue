d52e2a3c0f3e1d40dbe4c26712bd95ffd55ab0de
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2021-05-20 13:30:21 +1200

    Add pagination and filtering to DataTablePage.vue

--- client/src/pages/DataTablePage.vue
+++ client/src/pages/DataTablePage.vue
@@ -1,14 +1,55 @@
 <template>
-  <div>
-    <b-table striped
-             hover
-             :items="earthquakes"
-             :fields="fields"
-             :busy.sync="loadingStatus"
-             primary-key="publicid"
-    >
-    </b-table>
-  </div>
+  <b-container fluid>
+    <div>
+      <b-form-group
+          label="Filter"
+          label-for="filter-input"
+      >
+        <b-input-group>
+          <b-form-input
+              id="filter-input"
+              v-model="filter"
+              type="search"
+              placeholder="Type to Search"
+          />
+          <b-input-group-append>
+            <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </b-form-group>
+    </div>
+    <div>
+      <b-form-group
+          label="Per page"
+          label-for="per-page-select"
+      >
+        <b-form-select
+            id="per-page-select"
+            v-model="perPage"
+            :options="pageOptions"
+        />
+      </b-form-group>
+    </div>
+    <b-table
+        :items="earthquakes"
+        :fields="fields"
+        :busy.sync="loadingStatus"
+        :current-page="currentPage"
+        :per-page="perPage"
+        :filter="filter"
+        @filtered="onFiltered"
+        primary-key="publicid"
+        striped
+        hover
+    />
+    <div>
+      <b-pagination
+          v-model="currentPage"
+          :total-rows="totalRows"
+          :per-page="perPage"
+      />
+    </div>
+  </b-container>
 </template>
 
 <script>
@@ -19,14 +60,19 @@ export default {
 
   data: function () {
     return {
+      currentPage: 1,
+      totalRows: 0,
+      perPage: 10,
+      pageOptions: [10, 25, 50, 100],
+      filter: "",
       fields: [
-        { key: 'index', sortable: true },
-        { key: 'publicid', sortable: true, label: 'Public ID' },
-        { key: 'origintime', sortable: true, label: 'Origin Time'},
-        { key: 'depth', sortable: true },
-        { key: 'latitude', sortable: true },
-        { key: 'longitude', sortable: true },
-        { key: 'magnitude', sortable: true },
+        {key: 'index', sortable: true},
+        {key: 'publicid', sortable: true, label: 'Public ID'},
+        {key: 'origintime', sortable: true, label: 'Origin Time'},
+        {key: 'depth', sortable: true},
+        {key: 'latitude', sortable: true},
+        {key: 'longitude', sortable: true},
+        {key: 'magnitude', sortable: true},
       ]
     }
   },
@@ -38,12 +84,24 @@ export default {
       'startDate',
       'endDate'
     ]),
-    tableHeaders: function () {
-      if (this.earthquakes[0])
-        return Object.keys(this.earthquakes[0])
-      return null
+  },
+
+  watch: {
+    loadingStatus: function (isLoading) {
+      if (!isLoading) {
+        this.totalRows = this.earthquakes.length;
+        this.currentPage = 1;
+        this.filter = null;
+      }
     }
   },
+
+  methods: {
+    onFiltered(filteredItems) {
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
+    }
+  }
 }
 </script>
 
