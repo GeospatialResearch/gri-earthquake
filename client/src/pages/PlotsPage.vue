e39e7089b06c5ea40084f5bba915a24797ffa93b
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2021-05-21 14:33:13 +1200

    Add Plotly.js wrapper. Add placeholder plotly plots

--- client/src/pages/PlotsPage.vue
+++ client/src/pages/PlotsPage.vue
@@ -1,13 +1,41 @@
 <template>
-<div>
-  Plots page :)
-</div>
+  <b-container fluid>
+    <b-row>
+      <b-col>
+        <Plotly :data="plotData"/>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <Plotly :data="plotData"/>
+      </b-col>
+      <b-col>
+        <Plotly :data="plotData"/>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import {Plotly} from 'vue-plotly';
+
 export default {
   name: "PlotsPage",
   title: "Plots",
+
+  components: {
+    Plotly
+  },
+
+  data: function () {
+    return {
+      plotData: [{
+        x: [1, 2, 3, 4],
+        y: [10, 15, 13, 17],
+        type: "scatter"
+      }],
+    }
+  },
 
 }
 </script>
