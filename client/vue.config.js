3003ed7c82f4c3e2e617abd46adda1901aab9cc1
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2022-04-06 13:04:28 +1200

    Refactor file path constants in vue config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,4 @@
 const cesiumSource = 'node_modules/cesium/Source';
-const cesiumWorkers = '../Build/Cesium/Workers';
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
@@ -8,7 +7,7 @@ module.exports = {
   configureWebpack: {
     plugins: [
       new CopyWebpackPlugin([
-        {from: path.join(cesiumSource, cesiumWorkers), to: 'Workers'},
+        {from: path.join(cesiumSource, '../Build/Cesium/Workers'), to: 'Workers'},
         {from: path.join(cesiumSource, 'Assets'), to: 'Assets'},
         {from: path.join(cesiumSource, 'Widgets'), to: 'Widgets'}
       ]),
