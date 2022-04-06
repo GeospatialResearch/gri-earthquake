8381033a75d8df6762a481ac5cf5667e5e2300b1
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2022-04-07 07:47:48 +1200

    Fly to kaiapoi

--- client/src/pages/MapPage/MapViewer.vue
+++ client/src/pages/MapPage/MapViewer.vue
@@ -33,7 +33,16 @@ export default {
   },
 
   mounted() {
-    this.viewer = new Cesium.Viewer("cesiumContainer");
+    this.viewer = new Cesium.Viewer("cesiumContainer", {
+      imageryProvider: new Cesium.OpenStreetMapImageryProvider({
+        url: 'https://stamen-tiles.a.ssl.fastly.net/toner-lite/',
+        fileExtension: 'png',
+        credit: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.'
+      }),
+      terrainProvider: Cesium.createWorldTerrain({requestWaterMask: false})
+    });
+    this.viewer.camera.flyTo({destination: new Cesium.Cartesian3(-4612759.797689467, 593822.8330716471, -4365380.44925229)});
+
   }
 
 }
