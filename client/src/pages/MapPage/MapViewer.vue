04b2340ceb5f0a4d6c7db5d495ccbd0af6e49d9a
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2022-04-11 15:06:07 +1200

    Add 3d buildings

--- client/src/pages/MapPage/MapViewer.vue
+++ client/src/pages/MapPage/MapViewer.vue
@@ -38,11 +38,29 @@ export default {
         url: 'https://stamen-tiles.a.ssl.fastly.net/toner-lite/',
         fileExtension: 'png',
         credit: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.'
-      }),
-      terrainProvider: Cesium.createWorldTerrain({requestWaterMask: false})
+      })
     });
     this.viewer.camera.flyTo({destination: new Cesium.Cartesian3(-4612759.797689467, 593822.8330716471, -4365380.44925229)});
+    this.addDataSources();
+  },
 
+  methods: {
+    async addDataSources() {
+      const buildingDataSource = await Cesium.GeoJsonDataSource.load(
+          "kaiapoi_apr.geojson", {
+            stroke: Cesium.Color.HOTPINK,
+            fill: Cesium.Color.PINK.withAlpha(0.5),
+            strokeWidth: 3,
+          });
+      this.viewer.dataSources.add(buildingDataSource);
+      for (const buildingEntity of buildingDataSource.entities.values) {
+        buildingEntity.polygon.extrudedHeight = 4;
+      }
+      this.viewer.imageryLayers.addImageryProvider(
+          new Cesium.IonImageryProvider({assetId: 918127})
+      );
+
+    }
   }
 
 }
